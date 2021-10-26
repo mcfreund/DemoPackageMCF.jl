d07781c37a0665bc334d4368a4e9e8dc0e091cac
Documenter.jl <documenter@juliadocs.github.io>
2021-10-26 03:56:37 +0000

    build based on 2eceb44

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DemoPackageMCF","category":"page"},{"location":"#DemoPackageMCF","page":"Home","title":"DemoPackageMCF","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DemoPackageMCF.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DemoPackageMCF]","category":"page"}]
+}
